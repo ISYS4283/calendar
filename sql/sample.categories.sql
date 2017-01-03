738a681a6ff1b5cb4fe3eaa391c7bddc0c276810
Jeff Puckett <jeff@jeffpuckett.com>
2017-01-03 03:48:09 +0000

    add sample categories sql

---
+++ sql/sample.categories.sql
@@ -0,0 +1,10 @@
+INSERT INTO `categories` (`id`, `name`, `description`) VALUES
+(1, 'holiday', 'No class in lieu of university holiday.'),
+(2, 'class', 'WJWH0331 Trading Center Wed 12:00 PM - 2:50 PM'),
+(4, 'homework', NULL),
+(3, 'quiz', NULL),
+(5, 'blog', NULL),
+(6, 'Q&A', NULL),
+(7, 'project', NULL),
+(8, 'exam', NULL),
+(9, 'registrar', NULL);
